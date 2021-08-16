af5692e60cc0f790f269ca123a821551d9797fa8
Rayhan B <64879192+rayb26@users.noreply.github.com>
2021-08-16 11:09:37 -0400

    Update models.py

--- Website/models.py
+++ Website/models.py
@@ -1,14 +1,14 @@
+# Code created by Rayhan Biju 2021
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
+# Following code was never sent to production since we never deployed our login service. 
 class Alert(db.Model):
     title_id = db.Column(db.String(150), primary_key=True)
     title = db.Column(db.String(2000))
     episodes = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    # associate each user to note. Referencing 'user' class with id variable
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
