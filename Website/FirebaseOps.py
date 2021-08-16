55c62390f9f6de602cbbec1931fbc083f6ee4fba
Rayhan B <64879192+rayb26@users.noreply.github.com>
2021-08-16 11:06:33 -0400

    Update FirebaseOps.py

--- Website/FirebaseOps.py
+++ Website/FirebaseOps.py
@@ -2,22 +2,19 @@
 from firebase_admin import credentials
 import firebase_admin
 
-cred = credentials.Certificate('Website/event-fusion-bf6b1-firebase-adminsdk-xznez-482251e739.json')
+cred = credentials.Certificate('#your firebase table')
 
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://event-fusion-bf6b1.firebaseio.com'
+    'databaseURL': '#your database url'
 })
 
 
 def check_existing_email(email):
     from firebase import firebase
-    firebase = firebase.FirebaseApplication("https://fir-project-65f8b.firebaseio.com/", None)
-    result = firebase.get('/fir-project-65f8b/Alerts', '')
+    firebase = firebase.FirebaseApplication("#link to firebase table", None)
+    result = firebase.get('#path to firebase table', '')
 
     return email in str(result)
-
-    # return str(result).count(email) < 2
-
 
 
 def coming_soon_as_list():
@@ -25,10 +22,10 @@ def coming_soon_as_list():
     from firebase_admin import db
 
     coming_soon_list = []
-    firebase = firebase.FirebaseApplication("https://event-fusion-bf6b1.firebaseio.com/", None)
-    coming_soon_titles = db.reference('event-fusion-bf6b1').child('Coming-Soon').get()
+    firebase = firebase.FirebaseApplication("#your firebase table", None)
+    coming_soon_titles = db.reference('#firebase table name').child('Coming-Soon').get()
 
     for title in coming_soon_titles:
-        result = firebase.get('/event-fusion-bf6b1/Coming-Soon', title)
+        result = firebase.get('#path to firebase table', title)
         coming_soon_list.append(result.get('title'))
     return coming_soon_list
